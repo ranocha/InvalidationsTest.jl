3fab3cdad2a9cceedbf645be2ade26ab3d8b8942
Hendrik Ranocha <mail@ranocha.de>
2022-09-01 16:45:50 +0200

    show some GitHub actions environemnt variables

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,11 @@ using Test, InvalidationsTest
 
 @test InvalidationsTest.add(1, 2) == 3
 
+
+@show ENV["CI"]
+@show ENV["GITHUB_ACTION"]
+@show ENV["GITHUB_ACTION_PATH"]
+@show ENV["GITHUB_ACTION_REPOSITORY"]
+@show ENV["GITHUB_ACTIONS"]
+@show ENV["GITHUB_REPOSITORY"]
+@show ENV["GITHUB_REPOSITORY_OWNER"]
